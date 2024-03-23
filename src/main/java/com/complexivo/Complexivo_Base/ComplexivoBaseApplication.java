e59f2374cd85c7ce97017c5e27851ac6a2fbd9c2
lilySesh <liliana.cochancela.est@tecazuay.edu.ec>
2024-03-22 19:24:32 -0500

    subiendo

---
+++ src/main/java/com/complexivo/Complexivo_Base/ComplexivoBaseApplication.java
@@ -0,0 +1,13 @@
+package com.complexivo.Complexivo_Base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplexivoBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplexivoBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/complexivo/Complexivo_Base/ComplexivoBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.complexivo.Complexivo_Base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplexivoBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
