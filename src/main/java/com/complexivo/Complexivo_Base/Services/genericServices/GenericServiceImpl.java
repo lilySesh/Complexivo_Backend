9ae55ba98ab75fd4897e9315af3ad3aa2b297d83
Bryan Orellana <bryan.orellana.est@tecazuay.edu.ec>
2024-04-02 00:29:15 -0500

    sin seguridad

--- src/main/java/com/complexivo/Complexivo_Base/Services/genericServices/GenericServiceImpl.java
+++ src/main/java/com/complexivo/Complexivo_Base/Services/genericServices/GenericServiceImpl.java
@@ -9,13 +9,12 @@ import java.util.Optional;
 
 public  abstract class GenericServiceImpl <T, ID extends Serializable> implements GenericService<T, ID>{
     public abstract CrudRepository<T, ID> getDao();
-    @SuppressWarnings("null")
+
     @Override
     public T save(T Entity) {
         return getDao().save(Entity);
     }
 
-    @SuppressWarnings("null")
     @Override
     public T findById(ID id) {
         Optional<T> obj = getDao().findById(id);
@@ -32,7 +31,6 @@ public  abstract class GenericServiceImpl <T, ID extends Serializable> implement
         return list;
     }
 
-    @SuppressWarnings("null")
     @Override
     public void delete(ID id) {
         getDao().deleteById(id);
